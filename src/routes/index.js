b79eddef525164e033f61a6d07094b3b16412f5a
Sergio Salguero <salgueroflor1982@gmail.com>
2021-07-12 12:09:21 +0200

    create layouts, gitignore, partials

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,11 @@ const { Router } = require ('express');
 const router = Router()
 
 router.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('images')
+})
+
+router.get('/images/add', (req, res) => {
+    res.render('image_form')
 })
 
 module.exports = router
