e90712297f1481dbb84a82916708dd79ba24252b
Sergio Salguero <salgueroflor1982@gmail.com>
2021-07-13 00:35:15 +0200

    add cloudinary

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-console.log(process.env.MONGODB_URI)
-//mongoose.connect(, {
-//    useNewUrlParser: true
-//})
-//    .then(db => console.log('DB is connected'))
-//    .catch(err => console.error(err))
+
+mongoose.connect(process.env.MONGODB_URI, {
+   useNewUrlParser: true
+})
+   .then(db => console.log('DB is connected'))
+  .catch(err => console.error(err))
