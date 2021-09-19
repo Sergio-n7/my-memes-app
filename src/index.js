6653868d56ef5cc9fb386c830f0da34996171a18
sergiosalguero <salgueroflor1982@gmail.com>
2021-09-19 18:50:25 +0200

    publish to heroku

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+//if (process.env.NODE_ENV !== 'production') {
+//  require('dotenv').config()
+//}
 
 const app = require('./app')
 
-app.listen(app.get('port'), () => {
-  console.log(`Server on port  ${app.get('port')}`)
-  console.log('Enviroment: ', process.env.NODE_ENV)
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`Server on port  ${port}`)
+  //console.log('Enviroment: ', process.env.NODE_ENV)
 })
